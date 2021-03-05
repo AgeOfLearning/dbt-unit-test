72eb657597a7e84400b20445c7031f5ed15dd46e
Benjamin Ryon <benjamin.ryon@aofl.com>
2021-03-05 14:53:17 -0800

    Add manifest.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extra_reqs = {
 
 setup(
     name="dbt-unit-test",
-    version="0.0.3",
+    version="0.0.4",
     description=u"A tiny framework for testing reusable code inside of dbt models",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -32,9 +32,6 @@ setup(
     author_email="benjamin.ryon@aofl.com",
     url="https://github.com/AgeOfLearning/dbt-unit-test",
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
-    package_data={
-        "": ["*.yml", "*.sql"]
-    },
     include_package_data=True,
     zip_safe=False,
     install_requires=inst_reqs,
