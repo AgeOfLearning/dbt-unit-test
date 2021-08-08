5297ff0b7fef5f2bcb944309792099b2c0ef326c
Kevin Kramer <46426639+mcjack1967@users.noreply.github.com>
2021-08-08 07:54:29 +0200

    converts all varchars to binary if necessary

--- dbt_unit_test/templates/diff_macro.sql
+++ dbt_unit_test/templates/diff_macro.sql
@@ -1,9 +1,48 @@
+{%- macro print_list(list_to_print=none, indent=4) -%}
+
+    {%- for col_name in list_to_print -%}
+        {{- col_name | indent(indent) -}}{{ ",\n    " if not loop.last }}
+    {%- endfor -%}
+
+{%- endmacro -%}
+
 {% macro test_diff(model, test) %}
+
+{# converts all varchars to binary if necessary #}
+{%- set columns = adapter.get_columns_in_relation(model) -%}
+{%- set model_columns_to_select = [] -%}
+{%- set test_columns_to_select = [] -%}
+
+{% for column in columns %}
+    {% if column.dtype == 'BINARY' %}  
+        {% do test_columns_to_select.append("to_binary("~column.name~")") %}
+    {% else %}  
+        {% do test_columns_to_select.append(column.name) %}
+    {% endif %}  
+    {% do model_columns_to_select.append(column.name) %}
+{% endfor %}
+
 with extra_rows as (
-    SELECT * FROM {{ model }} EXCEPT SELECT * FROM {{ test }}
+    SELECT print_list(model_columns_to_select) 
+    FROM {{ model }} 
+    EXCEPT 
+    SELECT print_list(test_columns_to_select) 
+    FROM {{ test }}
 ), 
 missing_rows as (
-    SELECT * FROM {{ test }} EXCEPT SELECT * FROM {{ model }}
+    SELECT print_list(test_columns_to_select) 
+    FROM {{ test }} 
+    EXCEPT 
+    SELECT print_list(model_columns_to_select) 
+    FROM {{ model }}
 )
-SELECT * FROM extra_rows UNION ALL SELECT * FROM missing_rows
+SELECT * FROM extra_rows 
+UNION ALL 
+SELECT * FROM missing_rows
+{% endmacro %}
+{% macro drop_schema(name) %}
+{% set sql %}
+DROP SCHEMA IF EXISTS {{ name }}
+{% endset %}
+{% do run_query(sql) %}
 {% endmacro %}
